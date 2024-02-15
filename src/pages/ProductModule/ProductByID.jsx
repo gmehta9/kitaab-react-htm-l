c6be70257d6968349c25ab37c8d9b67aa8ad6b7b
Gmehta <gmehta.ice@gmail.com>
2024-02-15 22:39:45 +0530

    UI produt ui

--- src/pages/ProductModule/ProductByID.jsx
+++ src/pages/ProductModule/ProductByID.jsx
@@ -1,5 +1,6 @@
 import { Col, Image, Row } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import { srcPriFixLocal } from "../../helper/Helper";
 
 function ProductByID() {
     const location = useLocation()
@@ -8,12 +9,24 @@ function ProductByID() {
     return (
         <>
             <Row>
-                <Col xl={3}>
-                    <Image src="" />
+                <Col xl={4} md={4}>
+
+                    <Image
+                        className="w-100"
+                        src={`${srcPriFixLocal}product-img/main-image.jpg`} />
 
                 </Col>
-                <Col xl={9}>
-                    kk
+                <Col xl={8} md={8}>
+
+                    <div className="product">
+                        <div className="product-heading">Azarinth Healer: Book Three - A LitRPG Adventure Kindle</div>
+                        <div className="product-short-detail ">
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+                        </div>
+                        <div className="product-short-rating">
+
+                        </div>
+                    </div>
 
                 </Col>
             </Row>
