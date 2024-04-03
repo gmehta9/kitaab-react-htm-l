fc807f15b4d7e252fcbc9e4edd0306fa18fdf684
Gmehta <gmehta.ice@gmail.com>
2024-04-03 22:19:55 +0530

    cart added

--- src/context/Mcontext.context.js
+++ src/context/Mcontext.context.js
@@ -1,10 +1,39 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { axiosInstance, headers } from '../axios/axios-config';
+import Auth from '../auth/Auth';
 
 const MainContext = createContext(null);
 
 export const MainProvider = ({ children }) => {
 
     const [cartData, setCartData] = useState([]);
+
+    const cartApiHandlder = (method) => {
+
+        cartData.map(item => ({ id: item.id, qty: item.qty }));
+
+        axiosInstance[method]('cart', {
+            headers: {
+                ...headers,
+                Authorization: `Bearer ${Auth.token()}`,
+            }
+        }).then((res) => {
+            if (res) {
+                console.log(res);
+            }
+        }).catch((error) => {
+        });
+    }
+
+    useEffect(() => {
+
+        if (Auth.isUserAuthenticated()) {
+            cartApiHandlder('get')
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
 
     return (
         <MainContext.Provider
